5a2815de7b81d520449b61edb6f768f51c43d954
Satvik12221as <sanu8800971634@gmail.com>
2025-12-12 20:08:40 +0530

    model+accuracy

--- training.py
+++ training.py
@@ -79,4 +79,4 @@ print("Done!")
 
 
 byregymgitrngjubjutnjbnjnbnrenvjv er vhbutbhbvtnvnrjnvjkbvbby
-ubgtrbgnjrenjnuhguthnjbnjhbngjubtegbnnvujebnvekjnbkjjknjtneyugyuyu
+ubgtrbgnjrenjnuhguthnjbnjhbngjubtegbnnvujebnvekjnbkjjknjtneyugyuyuetnhjbt
