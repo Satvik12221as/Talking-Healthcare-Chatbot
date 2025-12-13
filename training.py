f1546be3f22741292ba652cc8a45235ccd930d5b
Satvik12221as <sanu8800971634@gmail.com>
2025-12-13 23:11:09 +0530

    model+accuracy

--- training.py
+++ training.py
@@ -77,4 +77,4 @@ model.save("chatbot_model.h5", hist)
 
 print("Done!")
  kjg
-junbyubyhjjgvvghvgyugyu hjbvgvetjnjetnjtnrjnbbfyjyletbtrtongjtt4njtntrjkbttrb
+junbyubyhjjgvvghvgyugyu hjbvgvetjnjetnjtnrjnbbfyjyletbtrtongjtt4njtntrjkbttrbttrvtr
